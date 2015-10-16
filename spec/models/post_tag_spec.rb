f50a66c028410d12c8936bb4d0777c674b4d85c7
Kyle Mellander <kylemellander@gmail.com>
2015-10-16 10:31:43 -0700

    Add Validation for PostTag

--- spec/models/post_tag_spec.rb
+++ spec/models/post_tag_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe PostTag do
   it { should belong_to :post }
   it { should belong_to :tag }
+  it { should validate_presence_of :post_id }
+  it { should validate_presence_of :tag_id }
 end
