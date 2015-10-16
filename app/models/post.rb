5945ea1562d7304d0e018e828d824fbcd8c92686
Kyle Mellander <kylemellander@gmail.com>
2015-10-16 10:25:27 -0700

    Add validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :post_tags
+  validates_presence_of :title, :content
+  validates :img, allow_blank: true, format: {
+    with: %r{\.gif|jpg|png|jpeg}i,
+    message: 'must be a url for gif, jpg, jpeg, or png image.'
+  }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Post do
   it { should have_many :post_tags }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :content }
+  it { should allow_value("frank.jpg").for(:img) }
+  it { should allow_value("").for(:img) }
+  it { should_not allow_value("frank.svg").for(:img) }
 end
